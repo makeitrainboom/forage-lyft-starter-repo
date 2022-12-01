52a07b251cc70f9956b28a88639fa81bba3f159b
kevin <kevinrhodes@Kevins-MBP.lan>
2022-12-01 14:58:30 -0800

    Expanded battery to include parent class

--- battery/nubbin_battery.py
+++ battery/nubbin_battery.py
@@ -1,15 +1,16 @@
-from abc import ABC
 from datetime import datetime
 
-from car import Car
+from battery import Battery
 
 
-class NubbinBattery(Car, ABC):
+
+class NubbinBattery(Battery):
     def __init__(self, last_service_date, current_date):
-        super().__init__(last_service_date)
+        self.last_service_date = last_service_date
         self.current_date = datetime.today().date()
 
-    def battery_should_be_serviced(self):
+
+    def needs_service(self):
         service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 4)
         if service_threshold_date < self.current_date:
             return True
