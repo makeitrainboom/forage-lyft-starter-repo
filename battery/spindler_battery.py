785c2936c0c9982c5ae67e7df1f31534610175cb
kevin <kevinrhodes@Kevins-MBP.lan>
2022-12-01 14:16:15 -0800

    Refactored model objects and added battery objects

---
+++ battery/spindler_battery.py
@@ -0,0 +1,17 @@
+from abc import ABC
+from datetime import datetime
+
+from car import Car
+
+
+class SpindlerBattery(Car, ABC):
+    def __init__(self, last_service_date, current_date):
+        super().__init__(last_service_date)
+        self.current_date = datetime.today().date()
+
+    def battery_should_be_serviced(self):
+        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 2)
+        if service_threshold_date < self.current_date:
+            return True
+        else:
+            return False
